12dee77a83c36da21f50f221b248461508ee0522
brahimchouih <brahim26cristal@gmail.com>
2021-12-29 11:49:20 +0100

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:brahimdb/screens/pomodoro_screen.dart';
 import 'package:brahimdb/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'screens/home_screen.dart';
 
@@ -11,6 +12,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
--- lib/utils/app_bar.dart
+++ lib/utils/app_bar.dart
@@ -5,32 +5,35 @@ import 'package:brahimdb/screens/home_screen.dart';
 import 'about.dart';
 
 AppBarCustmize(BuildContext context) {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: <Widget>[
-      Builder(
-        builder: (context) {
-          return IconButton(
-            icon: Image.asset("assets/icons/menu.png", height: height * 0.015),
-            onPressed: () {
-              Scaffold.of(context).openDrawer();
-            },
-          );
-        },
-      ),
-      IconButton(
-        icon: Image.asset("assets/icons/more.png", height: height * 0.03),
-        onPressed: () {
-          showDialog(
-              context: context,
-              builder: (_) {
-                return BackdropFilter(
-                  filter: kBlurFilter,
-                  child: aboutBrahim(_),
-                );
-              });
-        },
-      ),
-    ],
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 2.0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Builder(
+          builder: (context) {
+            return IconButton(
+              icon: Image.asset("assets/icons/menu.png", height: width * 0.03),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+        IconButton(
+          icon: Image.asset("assets/icons/more.png", height: width * 0.05),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (_) {
+                  return BackdropFilter(
+                    filter: kBlurFilter,
+                    child: aboutBrahim(_),
+                  );
+                });
+          },
+        ),
+      ],
+    ),
   );
 }
